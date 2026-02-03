a5999c45299298b9b748dd239ac83c9fcaef383c
Tirasp0l <120744803+SerafimLupan@users.noreply.github.com>
2026-02-04 00:25:55 +0200

    Update injection_sqli.sh

--- modules/web/injection_sqli.sh
+++ modules/web/injection_sqli.sh
@@ -3,4 +3,12 @@
 echo -e "${C6}[WEB ATTACK: INJECTION_SQLI]${NC}"
 echo -e "Reference: https://book.hacktricks.xyz/pentesting-web/sql-injection"
 echo -e "--------------------------------------------------"
+
+echo -ne "${C5}Enter Target URL: ${NC}"
+read target_url
+
+echo -e "\n${C4}[*] Running basic sqlmap scan...${NC}"
+sqlmap -u "$target_url" --batch --banner --current-db --random-agent --level=1 --risk=1
+
+echo -e "\n--------------------------------------------------"
 read -p "Press Enter to return..."
